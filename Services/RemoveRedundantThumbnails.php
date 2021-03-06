56423f9607f1c37de97ecd0b87f0927e7927a574
Andrew Tolpeko <andriush@mail.ru>
2021-03-06 14:18:00 +0300

    Removing redundant thumbnails in portions

--- Services/RemoveRedundantThumbnails.php
+++ Services/RemoveRedundantThumbnails.php
@@ -28,7 +28,8 @@ class RemoveRedundantThumbnails extends Service
         ?>
             <div class="wrap">
                 <h1>Remove Redundant Thumbnails</h1>
-                <button class="th_m_remove-redundant-button-js">Remove</button>
+                <button class="<?= $this->prefix ?>remove-redundant-button-js">Remove</button>
+                <div class="<?= $this->prefix ?>remove-redundant-result-js" data-page="1"></div>
             </div>
         <?php
     }
@@ -39,7 +40,8 @@ class RemoveRedundantThumbnails extends Service
             wp_enqueue_script($this->prefix . 'remove-redundant-ajax-handler', plugin_dir_url(__DIR__) . 'assets/js/remove-redundant-ajax-handler.js', ['jquery'], null, true);
             wp_localize_script($this->prefix . 'remove-redundant-ajax-handler', 'remove_redundant_ajax_handler',
                 [
-                    'ajaxurl' => admin_url('admin-ajax.php')
+                    'ajaxurl' => admin_url('admin-ajax.php'),
+                    'prefix' => $this->prefix
                 ]
             );
         });
@@ -59,9 +61,14 @@ class RemoveRedundantThumbnails extends Service
             'fields'         => 'ids',
         ];
 
+        if (isset($_POST['page'])) {
+            $attachmentArgs['paged'] = intval($_POST['page']);
+            $attachmentArgs['posts_per_page'] = 10;
+        }
+
         $attachmentQuery = new \WP_Query($attachmentArgs);
 
-        $existedImageSizeNames = [];
+/*        $existedImageSizeNames = [];
         global $_wp_additional_image_sizes;
 
         if (!empty($_wp_additional_image_sizes)) {
@@ -83,9 +90,14 @@ class RemoveRedundantThumbnails extends Service
             if (!empty($attachmentMeta['sizes'])) {
                 wp_update_attachment_metadata($attachmentId, $attachmentMeta);
             }
-        }
+        }*/
 
         if (DOING_AJAX) {
+            wp_send_json([
+                'page' => isset($_POST['page']) ? $_POST['page'] + 1 : 1,
+                'completed' => empty($attachmentQuery->found_posts)
+            ]);
+
             wp_die();
         }
     }
--- assets/js/remove-redundant-ajax-handler.js
+++ assets/js/remove-redundant-ajax-handler.js
@@ -1,17 +1,33 @@
 jQuery(document).ready(function() {
     jQuery('.th_m_remove-redundant-button-js').click(function (event) {
         event.preventDefault();
-
-        jQuery.ajax({
-            url: remove_redundant_ajax_handler.ajaxurl,
-            type: 'POST',
-            data: 'action=th_m_remove_redundant_thumbnails',
-            beforeSend: function(xhr) {
-                jQuery('.th_m_remove-redundant-button-js').text('Removing...');
-            },
-            success: function(data) {
-                jQuery('.th_m_remove-redundant-button-js').text('Remove');
-            }
-        });
+        sendAjaxQuery();
     });
 });
+
+function sendAjaxQuery() {
+    let removeButton = jQuery('.' + remove_redundant_ajax_handler.prefix + 'remove-redundant-button-js');
+    let resultElement = jQuery('.' + remove_redundant_ajax_handler.prefix + 'remove-redundant-result-js');
+    let page = resultElement.attr('data-page');
+
+    jQuery.ajax({
+        url: remove_redundant_ajax_handler.ajaxurl,
+        type: 'POST',
+        data: 'action=' + remove_redundant_ajax_handler.prefix + 'remove_redundant_thumbnails&page=' + page,
+        dataType: 'json',
+        beforeSend: function(xhr) {
+            if (parseInt(page) !== 1) {
+                removeButton.text('Removing...');
+            }
+        },
+        success: function(data) {
+            if (!data['completed']) {
+                resultElement.attr('data-page', parseInt(page) + 1);
+                sendAjaxQuery();
+            } else {
+                removeButton.text('Remove');
+                resultElement.text('Done!');
+            }
+        }
+    });
+}
